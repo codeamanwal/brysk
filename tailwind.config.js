dac7729623d46f530839628651680f2da118012f
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2024-06-13 10:28:24 -0400

    Initial push

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust the paths according to your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+};
