39a3c38ddd39c3f5b44636e0eea3c30edb4c0a2e
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2024-06-25 18:06:33 -0400

    update

---
+++ brysk-be/api/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+require("dotenv").config();
+const salesRoutes = require('./locationSales.js');
+const customerSalesRoutes = require('./customerSales.js');
+const inventoryAtLocationRoutes = require('./inventoryAtLocation.js');
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+// Enable CORS for all routes
+const corsOptions = {
+  origin: "*",
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+
+app.use('/api', salesRoutes);
+app.use('/api', customerSalesRoutes);
+app.use('/api', inventoryAtLocationRoutes);
+
+
+app.get("/", async(req, res) => {
+  res.send("Welcome!");
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
